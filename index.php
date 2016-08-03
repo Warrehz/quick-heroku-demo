70ae07ee31d65d7d757535db6aa37c337e943b70
Mical Juarez <mical.juarez@gmail.com>
2016-08-02 20:42:10 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Mical Juarez | Basic Portfolio</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  </head>
+
+  <body>
+
+      <header>
+
+        <h1>Mical Juarez</h1>
+
+
+        <div class="nav">
+          <ul>
+            <div class="nav-link"><li><a href="#">About</a></li></div>
+            <div class="nav-link"><li><a href="#">Portfolio</a></li></div>
+            <div class="nav-link"><li><a href="#">Contact</a></li></div>
+          </ul>
+        </div>
+
+      </header>
+
+    <main>
+
+      <div class="content">
+
+        <h2>About Me</h2>
+
+          <div id="profile-pic">
+
+            <img src="assets/images/man-at-bar.jpg" width="250px" height="200px">
+
+          </div>
+
+          <div id="content-text">
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+              ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+              voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+              non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+              ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+              voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+              non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+              ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+              voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+              non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+
+        </div>
+
+      <section>
+
+        <div class="sidebar">
+
+          <h3>Connect with Me</h3>
+
+            <ul>
+              <li><a href="#"><div class="social-media-button"><img src="assets/images/github-icon.png" height="50px" width="50px"/></div></a></li>
+              <li><a href="#"><div class="social-media-button"><img src="assets/images/linkedin-icon.png" height="50px" width="50px"/></div></a></li>
+              <li><a href="#"><div class="social-media-button"><img src="assets/images/stack-overflow-icon.png" height="50px" width="50px"/></div></a></li>
+              <li><a href="#"><div class="social-media-button"><img src="assets/images/twitter-icon.png" height="50px" width="50px"/></div></a></li>
+            </ul>
+
+        </div>
+
+      </section>
+
+    </main>
+
+      <footer>
+
+          <p>Copyright 2016 Mical Juarez</p>
+
+      </footer>
+
+    </body>
+
+</html>
